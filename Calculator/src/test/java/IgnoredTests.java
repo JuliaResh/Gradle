dfbe7af926c115cdd5f0bd27b5636507836e13c8
My username <lee.onore@gmail.com>
2015-12-14 15:28:13 +0300

    init

---
+++ Calculator/src/test/java/IgnoredTests.java
@@ -0,0 +1,59 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IgnoredTests {
+
+    private int value1;
+    private int value2;
+
+    @Test
+    public void testAdd() {
+        int total = 8;
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int sum = Calculation.add(value1, value2);
+        assertEquals(sum, total);
+    }
+
+    @Test
+    public void testAdd_ShouldFail() {
+        //int total = 18; //fail   2
+        int total = 8;    //pass
+        int sum = Calculation.add(value1, value2);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertEquals(sum, total);
+    }
+
+    @Test
+    public void testAddNeg() {
+        int total = -8;
+        int sum = Calculation.add(-value1, -value2);
+        assertEquals(sum, total);
+    }
+
+    @Ignore ("some ignore reason")
+    @Test
+    public void testIgnored1() {
+        int total = -8;
+        int sum = Calculation.add(-value1, -value2);
+        assertEquals(sum, total);
+    }
+
+    @Ignore ("some ignore reason")
+    @Test
+    public void testIgnored2() {
+        int total = -8;
+        int sum = Calculation.add(-value1, -value2);
+        assertEquals(sum, total);
+    }
+
+}
