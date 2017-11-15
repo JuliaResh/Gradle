6b0248fb814075a28289d09187f0a48e21ef26c3
Julia Reshetnikova <lee.onore@gmail.com>
2017-11-15 19:41:00 +0300

    fix compilation 2

--- Calculator/src/test/java/myPackage/MathTestSuite.java
+++ Calculator/src/test/java/myPackage/MathTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({MathTest.class, MathTest2.class})
+@Suite.SuiteClasses({myPackage.MathTest.class, myPackage.MathTest2.class})
 public class MathTestSuite extends TestSuite {
     public MathTestSuite() {
         addTestSuite(MathTest.class);
